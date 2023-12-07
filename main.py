28dbc0ef6c65e34ca08c1c77cd12bd38ce28e8ef
Victoria <wsmvictoria@gmail.com>
2023-12-07 20:37:57 +0800

    test boto

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import boto3
 
 st.title('mdc Clinical Notes')
+
+s3 = boto3.resource('s3')
+
+for bucket in s3.buckets.all():
+    st.write(bucket.name)

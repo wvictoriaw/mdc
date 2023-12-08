b0590f987de53b69ce5582338868b0383cfb246b
Victoria <wsmvictoria@gmail.com>
2023-12-08 16:32:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import boto3
 
 st.title('mdc Clinical Notes')
 
-s3 = boto3.resource('s3')
+s3 = boto3.resource('s3', aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_access_key)
 
 for bucket in s3.buckets.all():
     st.write(bucket.name)

5488226bc76449cc86ffd4e594c65e2ebee7caa1
Victoria <wsmvictoria@gmail.com>
2023-12-15 18:38:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,16 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import boto3
+from st_audiorec import st_audiorec
 
 st.title('mdc Clinical Notes')
+
+wav_audio_data = st_audiorec()
+
+if wav_audio_data is not None:
+    st.audio(wav_audio_data, format='audio/wav')
 
 s3 = boto3.resource('s3', aws_access_key_id=st.secrets['aws_access_key'], 
                     aws_secret_access_key=st.secrets['aws_secret_access_key'])
 
-for bucket in s3.buckets.all():
-    st.write(bucket.name)
+# s3.meta.client.upload_file('filename', 'mdc-transcribe', 'desired filename in s3')

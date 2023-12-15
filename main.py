45ae7a71708bf7f1d2b33f174f6088fba60fd184
Victoria <wsmvictoria@gmail.com>
2023-12-15 20:01:56 +0800

    audio record and upload

--- main.py
+++ main.py
@@ -2,16 +2,21 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import boto3
-from st_audiorec import st_audiorec
+from smart_open import open
+from audiorecorder import audiorecorder
+import datetime
 
 st.title('mdc Clinical Notes')
 
-wav_audio_data = st_audiorec()
-
-if wav_audio_data is not None:
-    st.audio(wav_audio_data, format='audio/wav')
-
-s3 = boto3.resource('s3', aws_access_key_id=st.secrets['aws_access_key'], 
+s3 = boto3.Session(aws_access_key_id=st.secrets['aws_access_key'], 
                     aws_secret_access_key=st.secrets['aws_secret_access_key'])
+
+audio = audiorecorder("Click to record", "Click to stop recording")
+
+if st.button('Upload recording') and len(audio) > 0:
+    filename = str(datetime.now())
+    url = f's3://mdc-transcribe/{filename}.mp3'
+    with open(url, 'wb', transport_params={'client': s3.client('s3')}) as fout:
+        audio.export(fout, format='mp3')
 
 # s3.meta.client.upload_file('filename', 'mdc-transcribe', 'desired filename in s3')
